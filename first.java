824dbbaf0b95a899af808449c3c421917a6c0042
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2023-12-10 21:47:35 +0300

    Update first.java

--- first.java
+++ first.java
@@ -7,32 +7,45 @@ public class ScoreInfo {
         numStudents = 1;
     }
 
-    public void increment() { numStudents++; }
-    public int getScore() { return score; }
-    public int getFrequency() { return numStudents; }
+    public void increment() 
+    { numStudents++; }
+
+    public int getScore() 
+    { return score; }
+
+    public int getFrequency() 
+    { return numStudents; }
 }
 
 
+// Stats class creates and maintains a database of student score information. The scores are sorted in sorted order in database
 public class Stats {
     private ArrayList<ScoreInfo> scoreList;
-    scorrList = new ArrayList<>();
+    // listed in increasing score order; no two ScoreInfo object contain the same score 
 
+    /*
+     * Records a score in the database, keeping the databse in increasing score order. If no other ScoreInfo object represents score, 
+     * a new ScoreInfo object representing score is added to the database; otherwise, the frequency in the ScoreInfo object representing
+     * score is incremented
+     * @param score - a score to be recorded in the list
+     * @return true - if a new ScoreInfo object representing score was added to the list; false otherwise
+    */
+    scorrList = new ArrayList<ScoreInfo>();
     public boolean record(int score) {
         boolean check = false;
         ScoreInfo s1 = new ScoreInfo(score);
-        scoreList.add(s1);
         for(int i = 0; i < scoreList.size(); i++) {
             if(score == scoreList.get(i).getScore()) {
-                s1.increment();
+                s1.increment(); 
             } else {
-            ScoreInfo s1 = new ScoreInfo(score);
+            scoreList.add(s1);
             check = true;
             }
         }
     
-        for(int j = 0; j < scorwList.size(); j++) {
+        for(int j = 0; j < scoreList.size(); j++) {
             for(int n = j; n < scoreList.size(); n++) {
-                if (scoreList.get(j+1) < scoreList.get(j)) {
+                if (scoreList.get(j+1).getScore() < scoreList.get(j).getScore()) {
                     int temp = scoreList.get(j);
                     scoreList.get(j) = scoreList.get(j+1),
                     scoreList.get(j+1) = temp;
@@ -41,11 +54,17 @@ public class Stats {
         }
         return check;
     }
-}
 
-
-public void recordScores(int[] stuScores) {
-    for(int i = 0; i < stuScores.length; i++) {
-        boolean b = record(stuStudents(i));
+    /*
+     * Records all scores in stuScores in the databse, keeping the databse in increasing order
+     * @param stuScores - an array of student test scores
+    */
+    public void recordScores(int[] stuScores) {
+        for(int i = 0; i < stuScores.length; i++) {
+            boolean b = record(stuStudents(i));
+        }
     }
 }
+
+
+
