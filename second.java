5820e50611afd31cfab4b11b3c9e2bfa0dfc27a5
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2023-12-10 21:47:39 +0300

    Create second.java

---
+++ second.java
@@ -0,0 +1,40 @@
+public class WordScrambler {
+    private String[] scrambledWords;
+
+    /*
+     * @param wordArr - an array of String objects
+     *      Precondition: wordArr.length is even
+    */
+    public WordScrambler(String[] wordArr) {
+        scrambledWords = mixedWords(wordArr);
+    }
+
+    /*
+     * @param word1 - a String of characters
+     * @param word2 - a String of characters
+     * @return a String that contains the first half of word1 and the second half of word2
+    */
+    private String recombine(String word1, String word2) {
+        int half1 = word1.length/2;
+        String substring1 = word1.substring(0, half1);
+
+        int half2 = word2.length/2;
+        String substring2 = word2.substring(half2);
+        
+        return (substring1 + substring2);
+    }
+
+    /*
+     * @param words an array of String objects 
+     *      Precondition: words.length is even
+     * @return an array of String objects created by recombining pairs of strings in array words 
+     *      Postcondition: the length of the returned array is words.length
+    */
+    private String[] mixedWords(String[] words) {
+        for(int i = 0; i < mixedWords.length; i+2) {
+            words[i] = recombine(words[i], words[i+1]);
+            wors[i+1] = recombine(words[i+1], words[i]);
+        }
+        return words[];
+    }
+}
